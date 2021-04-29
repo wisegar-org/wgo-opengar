20cf6014b61936ff63fa5829386fd77b2c038fa8
yariel <info@wisegar.org>
2021-04-29 21:32:18 +0200

    Remove build folder

--- build/models/EmailOptions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailOptions = void 0;
-class EmailOptions {
-}
-exports.EmailOptions = EmailOptions;
-//# sourceMappingURL=EmailOptions.js.map
--- build/services/data-services/BaseDataService.js
+++
@@ -1,9 +0,0 @@
-// import { Connection } from "typeorm";
-// import { getConnection } from "../../database/typeorm";
-// export class BaseDataService {
-//     public readonly _connection: Connection
-//     constructor() {
-//         this._connection = getConnection()
-//     }
-// }
-//# sourceMappingURL=BaseDataService.js.map
