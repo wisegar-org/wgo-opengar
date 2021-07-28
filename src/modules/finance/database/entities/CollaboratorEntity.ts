0d5f4e2529587d3648f03511668d5546bbeead5a
Liana Juliá <lianabeatriz93@gmail.com>
2021-07-28 11:04:40 -0400

    Remove pay to internet property from collaborator workflow

--- src/modules/finance/database/entities/CollaboratorEntity.ts
+++ src/modules/finance/database/entities/CollaboratorEntity.ts
@@ -1,38 +1,36 @@
-import { AccountEntity } from './AccountEntity'
-import { Entity, Column, BaseEntity, PrimaryColumn, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
-import { IssueEntity } from './IssueEntity'
-import { TransactionEntity } from './TransactionEntity'
+import { AccountEntity } from './AccountEntity';
+import { Entity, Column, BaseEntity, PrimaryColumn, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { IssueEntity } from './IssueEntity';
+import { TransactionEntity } from './TransactionEntity';
 
 @Entity()
 export class CollaboratorEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
-  id!: number
-  @Column({ default: 0 }) id_github: number
-  @Column() name: string
-  @Column() login: string
-  @Column() node_id: string
-  @Column() type: string
-  @Column() avatar_url: string
-  @Column() url: string
-  @Column() location: string
-  @Column() email: string
-  @Column() bio: string
-  @Column({ default: true }) isCollaborator!: boolean
+  id!: number;
+  @Column({ default: 0 }) id_github: number;
+  @Column() name: string;
+  @Column() login: string;
+  @Column() node_id: string;
+  @Column() type: string;
+  @Column() avatar_url: string;
+  @Column() url: string;
+  @Column() location: string;
+  @Column() email: string;
+  @Column() bio: string;
+  @Column({ default: true }) isCollaborator!: boolean;
 
-  @Column({ nullable: true, default: '' }) card_number?: string
-  @Column({ type: 'float', nullable: true, default: 0 }) pay_by_hours?: number
-  @Column({ type: 'float', nullable: true, default: 0 })
-  pay_to_internet?: number
-  @Column({ nullable: true, default: '' }) address?: string
+  @Column({ nullable: true, default: '' }) card_number?: string;
+  @Column({ type: 'float', nullable: true, default: 0 }) pay_by_hours?: number;
+  @Column({ nullable: true, default: '' }) address?: string;
 
   @OneToMany(() => IssueEntity, (issue) => issue.assignedToId)
-  issues?: IssueEntity[]
+  issues?: IssueEntity[];
 
   @OneToMany(() => AccountEntity, (acc) => acc.contributorId)
-  accounts?: AccountEntity[]
+  accounts?: AccountEntity[];
 
   @OneToMany(() => TransactionEntity, (issue) => issue.collaboratorId)
-  transactions?: TransactionEntity[]
+  transactions?: TransactionEntity[];
 
   constructor(
     numberId: number,
@@ -47,27 +45,25 @@ export class CollaboratorEntity extends BaseEntity {
     bio: string,
     card_number?: string,
     pay_by_hours?: number,
-    pay_to_internet?: number,
     address?: string
   ) {
-    super()
-    this.id_github = numberId
-    this.login = login
-    this.node_id = node_id
-    this.type = type
-    this.avatar_url = avatar_url
-    this.url = url
+    super();
+    this.id_github = numberId;
+    this.login = login;
+    this.node_id = node_id;
+    this.type = type;
+    this.avatar_url = avatar_url;
+    this.url = url;
 
-    this.name = name
-    this.location = location
-    this.email = email
-    this.bio = bio
+    this.name = name;
+    this.location = location;
+    this.email = email;
+    this.bio = bio;
 
-    this.card_number = card_number
-    this.pay_by_hours = pay_by_hours
-    this.pay_to_internet = pay_to_internet
-    this.address = address
+    this.card_number = card_number;
+    this.pay_by_hours = pay_by_hours;
+    this.address = address;
   }
 }
 
-export default CollaboratorEntity
+export default CollaboratorEntity;
