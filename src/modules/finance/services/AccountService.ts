71724abd0144129b84acc0640938c24e672386d9
Liana Juliá <lianabeatriz93@gmail.com>
2021-08-11 00:26:13 -0400

    Add accounting template and send link to workflow

--- src/modules/finance/services/AccountService.ts
+++ src/modules/finance/services/AccountService.ts
@@ -7,8 +7,17 @@ import { CollaboratorService, ProjectService } from '.';
 import { TransactionService } from './TransactionService';
 import { OrganizationDataService } from './OrganizationDataService';
 import { ExpensesService } from './ExpensesService';
-import { Context } from '@wisegar-org/wgo-opengar-core';
+import { Context, GetPublicKey } from '@wisegar-org/wgo-opengar-core';
+import { TemplateTokens } from '../utils/models';
+import OrganizationDataEntity from '../database/entities/OrganizationDataEntity';
+import jsonwebtoken from 'jsonwebtoken';
+import { TemplateService } from './TemplateService';
+import { EmailService } from './EmailService';
+import { v4 as uuidv4 } from 'uuid';
 
+const PATH_ACCOUTING = '/content/AccountingTemplate.html';
+const PATH_EMAIL_ACCOUNTING = '/content/AccountingEmailTemplate.html';
+const PATH_PAGE_TEMPLATE = '/content/TemplatePage.html';
 export class AccountService {
   private connection: Connection;
   private accountConnection: Repository<AccountEntity>;
@@ -16,10 +25,12 @@ export class AccountService {
   private projectService: ProjectService;
   private repositoryService: RepositoryService;
   private issueService: IssueService;
-  private transactionService: TransactionService;
   private expenseService: ExpensesService;
+  private templateService: TemplateService;
+  private emailService: EmailService;
 
   private orgDataService: OrganizationDataService;
+  path: string;
 
   constructor(userContext?: Context) {
     // debugger
@@ -29,9 +40,10 @@ export class AccountService {
     this.projectService = new ProjectService();
     this.repositoryService = new RepositoryService();
     this.issueService = new IssueService(userContext);
-    this.transactionService = new TransactionService(userContext);
     this.expenseService = new ExpensesService(userContext);
     this.orgDataService = new OrganizationDataService();
+    this.templateService = new TemplateService();
+    this.emailService = new EmailService();
   }
 
   async Add(
@@ -181,5 +193,109 @@ export class AccountService {
       }
     }
     return false;
+  }
+
+  async loadTemplate(): Promise<string> {
+    return this.templateService.getTemplateContent(PATH_ACCOUTING);
+  }
+
+  async saveTemplate(value: string) {
+    return this.templateService.setTemplateContent(PATH_ACCOUTING, value);
+  }
+
+  async sendAccountingLink(id: number, urlApi: string) {
+    const accounting = await this.accountConnection.findOne({
+      where: {
+        id: id,
+      },
+      relations: ['contributor', 'projects', 'repos'],
+    });
+
+    accounting.issues = await this.issueService.getIssuesFromAccount(id);
+
+    const organization = await this.orgDataService.getOrganizationData();
+    const pageTemplate = this.templateService.getTemplateContent(PATH_PAGE_TEMPLATE);
+    const emailTemplate = this.templateService.getTemplateContent(PATH_EMAIL_ACCOUNTING);
+    const template = await this.loadTemplate();
+    const tokensTemplate = this.getAccountingTokens(accounting, organization, template);
+    const tokensTable = this.getAccountingTableTokens(accounting, organization);
+    const nameFile = `${uuidv4()}.html`;
+    const path = this.templateService.createDocument(nameFile, pageTemplate, tokensTemplate, tokensTable);
+    const secret = GetPublicKey();
+    const token = jsonwebtoken.sign(
+      { clientId: accounting.contributorId, nameDoc: nameFile, billId: accounting.id },
+      secret
+    );
+
+    const urlBill = `${urlApi}${path}?token=${token}`;
+    const emailTokens = this.getAccountingEmailTokens(accounting.payment_code, urlBill);
+    this.emailService.sendEmail(
+      organization.name,
+      accounting.contributor.email,
+      // `${accounting.contributor.name} <${accounting.contributor.email}>`,
+      `Accounting ${accounting.payment_code}`,
+      this.templateService.replaceTokens(emailTemplate, emailTokens)
+    );
+    console.log(urlBill);
+    return urlBill;
+  }
+
+  getAccountingEmailTokens(name: string, link: string) {
+    const tokens = <TemplateTokens>{};
+    tokens['[NAME]'] = name;
+    tokens['[BILL_LINK]'] = link;
+    return tokens;
+  }
+
+  getAccountingTokens(accounting: AccountEntity, organization: OrganizationDataEntity, template: string) {
+    const collaborator = accounting.contributor;
+    let internet = (accounting.total_hours * 60) / 1024;
+    internet = Math.round(internet * 100) / 100;
+    const date = accounting.date;
+
+    const total_hours = accounting.total_hours;
+    const total_issues = total_hours * accounting.pay_by_hours;
+    const total_internet = total_hours * accounting.pay_to_internet * accounting.internet_cost;
+    let taxes = accounting.taxes;
+    const total = total_issues + total_internet;
+
+    const tokens = <TemplateTokens>{};
+    tokens['[TITLEPAGE]'] = `${accounting.payment_code}`;
+    tokens['[BODYPAGE]'] = template;
+    tokens['[COLLABORATOR_NAME]'] = `${collaborator.name}`;
+    tokens['[COLLABORATOR_ADDRESS]'] = `${collaborator.address}`;
+    tokens['[COLLABORATOR_EMAIL]'] = `${collaborator.email}`;
+    tokens['[ACCOUNTING_ID]'] = `${accounting.payment_code}`;
+    tokens['[ACCOUNTING_DATE]'] = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+    tokens['[ACCOUNTING_HOURS]'] = `${accounting.total_hours}`;
+    tokens['[ACCOUNTING_INTERNET]'] = `${internet}Gb`;
+    tokens['[ACCOUNTING_SUBTOTAL]'] = `${total_issues}${organization.accountingCoin}`;
+    tokens['[ACCOUNTING_TAXES]'] = `${taxes}${organization.accountingCoin}`;
+    tokens['[ACCOUNTING_TOTAL]'] = `${total - taxes}${organization.accountingCoin}`;
+    tokens['[ACCOUNTING_OBSERVATION]'] = `${accounting.details}`;
+    tokens['[ACCOUNTING_DETAIL]'] = `${accounting.payment_comment}`;
+    tokens['[ORGANIZATION_NAME]'] = `${organization.name}`;
+    tokens['[ORGANIZATION_DESCRIPTION]'] = `${organization.description}`;
+    tokens['[ORGANIZATION_ADDRESS]'] = `${organization.address}`;
+    tokens['[ORGANIZATION_PHONE]'] = `${organization.phone}`;
+    tokens['[ORGANIZATION_EMAIL]'] = `${organization.email}`;
+    tokens['[ORGANIZATION_INTERNET_PRICE]'] = `${total_internet}${organization.accountingCoin}`;
+
+    return tokens;
+  }
+
+  getAccountingTableTokens(accounting: AccountEntity, organization: OrganizationDataEntity) {
+    const collaborator = accounting.contributor;
+    const tokens: TemplateTokens[] = accounting.issues.map((issue, index) => {
+      const tokensBill = <TemplateTokens>{};
+      tokensBill['[INDEX]'] = `${index + 1}`;
+      tokensBill['[ORGANIZATION_UNIT]'] = `${organization.accountingUnit}`;
+      tokensBill['[ISSUE_HOURS]'] = `${issue.hours}`;
+      tokensBill['[ISSUE_PRICE]'] = `${accounting.pay_by_hours}${organization.accountingCoin}`;
+      tokensBill['[ISSUE_TITLE]'] = `${issue.number} - ${issue.title}`;
+      tokensBill['[ISSUE_PRICE_TOTAL]'] = `${issue.hours * accounting.pay_by_hours}${organization.accountingCoin}`;
+      return tokensBill;
+    });
+    return tokens;
   }
 }
