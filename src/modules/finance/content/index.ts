2de61918ad2cfb655f276d5548e5eae890a8c74e
Liana Juliá <lianabeatriz93@gmail.com>
2021-08-11 00:22:30 -0400

    Add export public paths to index content

--- src/modules/finance/content/index.ts
+++ src/modules/finance/content/index.ts
@@ -1,21 +1,38 @@
-import { Connection } from "typeorm";
-import CollaboratorEntity from "../database/entities/CollaboratorEntity";
+import { Connection } from 'typeorm';
+import CollaboratorEntity from '../database/entities/CollaboratorEntity';
+import { GetPublicPathKey, GetPublicReportPath } from '../services/SettingsService';
+import { existsSync, mkdirpSync } from 'fs-extra';
 
 export async function CheckCollaboratosId(conn: Connection) {
-    const collaboratorConnection = conn.getRepository(CollaboratorEntity)
-    
-    const collaborators = await collaboratorConnection.find({
-        where: {
-            isCollaborator: true
-        }
+  const collaboratorConnection = conn.getRepository(CollaboratorEntity);
+
+  const collaborators = await collaboratorConnection.find({
+    where: {
+      isCollaborator: true,
+    },
+  });
+
+  await Promise.all(
+    collaborators.map(async (coll) => {
+      if (coll.id_github === 0) {
+        coll.id_github = coll.id;
+        await collaboratorConnection.manager.save(coll);
+      }
     })
+  );
 
-    await Promise.all(collaborators.map(async (coll) => {
-        if(coll.id_github === 0){
-            coll.id_github = coll.id
-            await collaboratorConnection.manager.save(coll)
-        }
-    }))
+  return true;
+}
 
-    return true
+export function exportPublicPaths() {
+  const publicPath = GetPublicPathKey();
+  if (!existsSync(publicPath)) {
+    mkdirpSync(publicPath);
+    console.log('Create public folder');
+  }
+  const publicReportPath = GetPublicReportPath();
+  if (!existsSync(publicReportPath)) {
+    mkdirpSync(publicReportPath);
+    console.log('Create public folder');
+  }
 }
