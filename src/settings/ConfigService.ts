d5658993982b56bd63c0470c94c827fdee8e2521
Liana Juliá <lianabeatriz93@gmail.com>
2021-08-25 23:36:12 -0400

    Add Media model and resolver

---
+++ src/settings/ConfigService.ts
@@ -0,0 +1,35 @@
+import { cypherData, decypherData } from '@wisegar-org/wgo-opengar-core';
+import { join } from 'path';
+
+export const PUBLIC_FOLDER_NAME = 'public';
+export const PRIVATE_FOLDER_NAME = 'private';
+export const FILES_STORAGE_FOLDER_NAME = 'files';
+
+export const GetWebRootKey = () => {
+  if (process.env.APP_WEB_ROOT) return process.env.APP_WEB_ROOT;
+  throw 'Impossible to get value from APP_WEB_ROOT environment key';
+};
+export const GetPackageJsonKey = () => {
+  const APP_WEB_ROOT = GetWebRootKey();
+  return join(APP_WEB_ROOT, 'package.json');
+};
+
+export const GetPublicPathKey = () => {
+  const APP_WEB_ROOT = GetWebRootKey();
+  return join(APP_WEB_ROOT, PUBLIC_FOLDER_NAME);
+};
+
+export const GetPrivatePathKey = () => {
+  const APP_WEB_ROOT = GetWebRootKey();
+  return join(APP_WEB_ROOT, PRIVATE_FOLDER_NAME);
+};
+
+export const GetPublicFilesPath = () => {
+  const pathPublic = GetWebRootKey();
+  return join(pathPublic, FILES_STORAGE_FOLDER_NAME);
+};
+
+export const GetPrivateFilesPath = () => {
+  const pathPrivate = GetWebRootKey();
+  return join(pathPrivate, FILES_STORAGE_FOLDER_NAME);
+};
