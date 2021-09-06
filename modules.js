c03550f67569cb98062829d76bdbbd766334f9ef
yarielre <yariel.re@gmail.com>
2021-09-06 21:04:48 +0200

    Add modules settings file

---
+++ modules.js
@@ -0,0 +1,12 @@
+{
+  modules: [
+    {
+      id: 'agv',
+      build: './src/modules/agv/build.js',
+    },
+    {
+      id: 'finance',
+      build: './src/modules/finance/build.js',
+    },
+  ];
+}
