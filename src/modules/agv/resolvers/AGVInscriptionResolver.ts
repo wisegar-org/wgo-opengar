59e583b4524b081f9e3d802a1749bb836d716cab
Liana Juliá <lianabeatriz93@gmail.com>
2021-09-08 14:26:00 -0400

    Add AGV Inscription Resolver and Service

---
+++ src/modules/agv/resolvers/AGVInscriptionResolver.ts
@@ -0,0 +1,34 @@
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { DBConector } from '../../../database/DBConector';
+import { AGVEventInput } from '../modules/agvEvent/AGVEventInputs';
+import { AGVInscriptionInput } from '../modules/agvInscription/AGVInscriptionInputs';
+import { AGVInscriptionResponse } from '../modules/agvInscription/AGVInscriptionResponses';
+import { AGVInscriptionService } from '../services/AGVInscriptionService';
+
+@Resolver()
+export class AGVInscriptionResolver {
+  private inscriptionService: AGVInscriptionService;
+
+  /**
+   *
+   */
+  constructor() {
+    const conn = DBConector.GetConnection();
+    this.inscriptionService = new AGVInscriptionService(conn);
+  }
+
+  @Query(() => [AGVInscriptionResponse])
+  async agvAllInscriptions(@Arg('urlApi') urlApi: string) {
+    // return await this.inscriptionService.all(urlApi);
+  }
+
+  @Mutation(() => Boolean)
+  async agvCreateInscription(@Arg('data') data: AGVInscriptionInput) {
+    return await this.inscriptionService.create(data);
+  }
+
+  @Mutation(() => Boolean)
+  async agvModifyInscription(@Arg('data') data: AGVEventInput) {
+    // return await this.inscriptionService.modify(data);
+  }
+}
