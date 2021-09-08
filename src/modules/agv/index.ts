39ddc62e7f184acc460c58e8bcd793c7ac5d37c6
Liana Juliá <lianabeatriz93@gmail.com>
2021-09-08 14:26:26 -0400

    Add AGVInscriptionResolver and AGVInscriptionEntity to AGV module config

--- src/modules/agv/index.ts
+++ src/modules/agv/index.ts
@@ -1,14 +1,15 @@
-import { CreatePathInPrivateFolder, CreatePathInPublicFolder } from '../../settings/ConfigService';
 import AGVEventEntity from './database/entities/AGVEventEntity';
+import { AGVInscriptionEntity } from './database/entities/AGVInscriptionEntity';
 import AGVPollEntity from './database/entities/AGVPollEntity';
 import { AGVEventResolver, AGVPollResolver } from './resolvers';
+import { AGVInscriptionResolver } from './resolvers/AGVInscriptionResolver';
 
 export const getAGVResolvers = () => {
-  return [AGVEventResolver, AGVPollResolver];
+  return [AGVEventResolver, AGVPollResolver, AGVInscriptionResolver];
 };
 
 export const getAGVEntities = () => {
-  return [AGVEventEntity, AGVPollEntity] as any[];
+  return [AGVEventEntity, AGVPollEntity, AGVInscriptionEntity] as any[];
 };
 
 export const AGV_MODULE = 'agv';
