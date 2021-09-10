6625419c726d3b778a61d422e72c469300aba2a4
Liana Juliá <lianabeatriz93@gmail.com>
2021-09-10 16:00:29 -0400

    Add web root param to build script

--- build.js
+++ build.js
@@ -5,7 +5,7 @@ console.log('\x1b[34m', 'BUILDING & DEPLOYING FINANCE API');
 const BUILD_ARGS = process.argv.slice(2);
 console.log('\x1b[34m', `BUILD_ARGS: ${BUILD_ARGS}`);
 
-if (!BUILD_ARGS || BUILD_ARGS.length < 3)
+if (!BUILD_ARGS || BUILD_ARGS.length < 4)
   throw 'Params pipeline not provided. Please provide: environment server_port module_to_build';
 
 const NODE_ENV = BUILD_ARGS[0] ? BUILD_ARGS[0] : 'development';
@@ -17,6 +17,9 @@ console.log('\x1b[34m', `PORT_ENV: ${PORT_ENV}`);
 const MODULE_ENV = BUILD_ARGS[2] ? BUILD_ARGS[2] : 'wgo';
 console.log('\x1b[34m', `MODULE_ENV: ${MODULE_ENV}`);
 
+const APP_WEB_ROOT_ENV = BUILD_ARGS[3] ? BUILD_ARGS[2] : 'wgo';
+console.log('\x1b[34m', `APP_WEB_ROOT_ENV: ${APP_WEB_ROOT_ENV}`);
+
 const mainmodule = require(`./deploy`);
-mainmodule.buildApi(MODULE_ENV, NODE_ENV, PORT_ENV);
+mainmodule.buildApi(MODULE_ENV, NODE_ENV, PORT_ENV, APP_WEB_ROOT_ENV);
 mainmodule.buildClient(MODULE_ENV, NODE_ENV);
