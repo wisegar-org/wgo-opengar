4b66e8fda90b3eae973fb8d7849a4f6fa1dd1f2d
Liana Juliá <lianabeatriz93@gmail.com>
2021-09-10 16:18:55 -0400

    Fix build WEB_ROOT_ENV param

--- build.js
+++ build.js
@@ -17,9 +17,9 @@ console.log('\x1b[34m', `PORT_ENV: ${PORT_ENV}`);
 const MODULE_ENV = BUILD_ARGS[2] ? BUILD_ARGS[2] : 'wgo';
 console.log('\x1b[34m', `MODULE_ENV: ${MODULE_ENV}`);
 
-const APP_WEB_ROOT_ENV = BUILD_ARGS[3] ? BUILD_ARGS[2] : 'wgo';
-console.log('\x1b[34m', `APP_WEB_ROOT_ENV: ${APP_WEB_ROOT_ENV}`);
+const WEB_ROOT_ENV = BUILD_ARGS[3] ? BUILD_ARGS[3] : '/users/yarielre/web';
+console.log('\x1b[34m', `WEB_ROOT_ENV: ${WEB_ROOT_ENV}`);
 
 const mainmodule = require(`./deploy`);
-mainmodule.buildApi(MODULE_ENV, NODE_ENV, PORT_ENV, APP_WEB_ROOT_ENV);
+mainmodule.buildApi(MODULE_ENV, NODE_ENV, PORT_ENV, WEB_ROOT_ENV);
 mainmodule.buildClient(MODULE_ENV, NODE_ENV);
