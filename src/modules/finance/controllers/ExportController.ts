d876d845e99ef7b4094e52aaead490ba524137c4
Yariel Rodriguez <info@wisegar.org>
2021-09-14 13:40:06 -0400

    Add use EmailServer core method

--- src/modules/finance/controllers/ExportController.ts
+++ src/modules/finance/controllers/ExportController.ts
@@ -1,8 +1,13 @@
 import { Express } from 'express';
 import { Connection } from 'typeorm';
-import { AuthorizeUserRol } from '@wisegar-org/wgo-opengar-core';
+import {
+  AuthorizeUserRol,
+  EmailServer,
+  EmailOptions,
+  GetEmailAppAddressKey,
+  GetEmailAppAddressNameKey,
+} from '@wisegar-org/wgo-opengar-core';
 import { GenerateAccountHTML } from '../services/AccountDetailsToHTML';
-import { SendEmail } from '../services/SendEmail';
 import { ReadStream } from 'fs-extra';
 export const ExportController = (app: Express, conn: Connection) => {
   app.get('/api/exportPdf/:id', AuthorizeUserRol(), async (req, res) => {
@@ -16,7 +21,24 @@ export const ExportController = (app: Express, conn: Connection) => {
   app.post('/api/sendEmail', AuthorizeUserRol(), async (req, res) => {
     const { idAccounting, to, subject, body } = req.body;
     GenerateAccountHTML(idAccounting, (doc: ReadStream) => {
-      SendEmail(to, subject, body, doc);
+      const emailServer = new EmailServer();
+      emailServer
+        .send(<EmailOptions>{
+          to: to,
+          from: `${GetEmailAppAddressNameKey()} <${GetEmailAppAddressKey()}>`,
+          subject: subject,
+          html: body,
+          attachments: [
+            {
+              // utf-8 string as an attachment
+              filename: 'invoice.pdf',
+              content: doc,
+            },
+          ],
+        })
+        .then((result) => {
+          console.log('Message sent: %s', result.message);
+        });
     });
   });
 };
