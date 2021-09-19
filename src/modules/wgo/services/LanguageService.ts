3751cb214f9772d3ecd74bdc8446da65f70ca0ba
Yariel Rodriguez <info@wisegar.org>
2021-09-19 16:16:53 -0400

    Add LanguageEntity models, resolvers, and service

---
+++ src/modules/wgo/services/LanguageService.ts
@@ -0,0 +1,64 @@
+import { MediaEntity } from '@wisegar-org/wgo-opengar-core';
+import { Connection, Repository } from 'typeorm';
+import { LanguageEntity } from '../database/entities/LanguageEntity';
+
+export interface ILanguage {
+  id: number;
+  code: string;
+  disabled: boolean;
+  default: boolean;
+  logoId: number;
+}
+
+export class LanguageService {
+  languageRepository: Repository<LanguageEntity>;
+  mediaRepository: Repository<MediaEntity>;
+  /**
+   *
+   */
+  constructor(conn: Connection) {
+    this.languageRepository = conn.getRepository(LanguageEntity);
+    this.mediaRepository = conn.getRepository(MediaEntity);
+  }
+
+  async all(whitRelations: boolean = false) {
+    const language = await this.languageRepository.find({
+      relations: whitRelations ? ['logo'] : [],
+    });
+
+    return language;
+  }
+
+  async create(language: ILanguage) {
+    let lang = await this.languageRepository.findOne({
+      where: { code: language.code },
+    });
+    if (!!language) return false;
+
+    lang = new LanguageEntity();
+    return !!(await this.setProperties(lang, language));
+  }
+
+  async modify(language: ILanguage) {
+    let lang = await this.languageRepository.findOne({
+      where: {
+        id: language.id,
+      },
+    });
+    if (!language) return false;
+    return !!(await this.setProperties(lang, language));
+  }
+
+  private async setProperties(lang: LanguageEntity, language: ILanguage) {
+    lang.code = language.code;
+    lang.default = language.default;
+    lang.disabled = language.disabled;
+    if (language.logoId) {
+      const media = await this.mediaRepository.findOne({
+        id: language.logoId,
+      });
+      lang.logo = media;
+    }
+    return await this.languageRepository.manager.save(lang);
+  }
+}
