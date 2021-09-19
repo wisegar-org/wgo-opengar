fecc2430df83323db41e3f9a877b9ba4a664f128
Yariel Rodriguez <info@wisegar.org>
2021-09-19 16:37:04 -0400

    Remove unused files in root path

--- src/graphql/resolvers/index.ts
+++
@@ -1,9 +0,0 @@
-import { AppResolver } from './AppResolver';
-import { EmailResolver } from './EmailResolver';
-import { MediaResolver } from './MediaResolver';
-import { RoleResolver } from './RoleResolver';
-import { UserResolver } from './UserResolver';
-
-export const getWGOResolvers = () => {
-  return [AppResolver, EmailResolver, MediaResolver, RoleResolver, UserResolver];
-};
--- src/graphql/types/responses/RoleResponseGQL.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectType } from "type-graphql";
-import { GenericArrayResponseGQL, GenericResponseGQL } from "./ResponseGQL";
-import { RoleGQL } from '../inputs/RoleInputGQL';
-
-@ObjectType()
-export class RoleResponseGQL extends GenericResponseGQL(RoleGQL) {}
-
-@ObjectType()
-export class RoleListResponseGQL extends GenericArrayResponseGQL(RoleGQL) { }
