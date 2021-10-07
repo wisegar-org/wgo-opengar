b2542fc9cffb785a5b88de85ca52a95288b3bd7f
Liana Julia <lianabeatriz93@gmail.com>
2021-10-07 04:59:09 -0400

    Fix test html to pdf package

--- src/modules/finance/services/AccountDetailsToHTML.ts
+++ src/modules/finance/services/AccountDetailsToHTML.ts
@@ -5,6 +5,7 @@ import { IssueEntity } from '../database/entities/IssueEntity';
 import { OrganizationDataEntity } from '../database/entities/OrganizationDataEntity';
 import { ReadStream } from 'fs-extra';
 import { exportHTMLToPdf } from '@wisegar-org/wgo-opengar-core';
+import { Readable } from 'stream';
 
 class GenerateAccountingPDF {
   accountigService: AccountService;
@@ -30,23 +31,47 @@ class GenerateAccountingPDF {
       templateAcc.body,
       templateAcc.styleTemplate.body
     );
+    // await this.accountigService.parseTemplateService.createDocument('./', 'temp2.html', body);
     return body;
   }
 }
 
 export async function GenerateAccountHTML(id: number, callback: (doc: ReadStream) => void) {
   const generatePDF = new GenerateAccountingPDF();
-  const content = await generatePDF.generatePDF(id);
+  const content = { content: await generatePDF.generatePDF(id) };
 
-  await exportHTMLToPdf(
-    content,
-    {
-      format: 'a4',
-      margin: {
-        top: 50,
-        bottom: 50,
-      },
-    },
-    callback
-  );
+  const html_to_pdf = require('html-pdf-node');
+  html_to_pdf
+    .generatePdf(content, {
+      args: ['--enable-viewport', '--extension-content-verification'],
+      format: 'A4',
+      preferCSSPageSize: true,
+      // margin: {
+      //   top: 120,
+      //   bottom: 120,
+      //   right: 120,
+      //   left: 120,
+      // },
+    })
+    .then((pdfBuffer) => {
+      const readable = new Readable();
+      readable._read = () => {};
+      readable.push(pdfBuffer);
+      readable.push(null);
+      callback(readable as any);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  // await exportHTMLToPdf(
+  //   content,
+  //   {
+  //     format: 'a4',
+  //     margin: {
+  //       top: 50,
+  //       bottom: 50,
+  //     },
+  //   },
+  //   callback
+  // );
 }
