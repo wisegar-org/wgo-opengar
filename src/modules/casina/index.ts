9539e3ea8e0109811b7a5ef26c72e0c6ffb41823
Liana Julia <lianabeatriz93@gmail.com>
2021-10-11 17:58:50 -0400

    Add Casina index content model, inputs and resolvers

--- src/modules/casina/index.ts
+++ src/modules/casina/index.ts
@@ -1,18 +1,17 @@
 import { ContactEntity } from './database/entities/ContactEntity';
 import { ContactResolver } from './modules/Contact/ContactResolver';
+import CasinaModuleEntity from './database/entities/CasinaModuleEntity';
+import { IndexContentResolver } from './modules/IndexContent/IndexContentResolver';
+
 export { InitializeCasinaMiddlewares } from './middleware';
 export { SeederCasina } from './seeder';
 
 export const getCasinaResolvers = () => {
-  return [
-    ContactResolver
-  ];
+  return [ContactResolver, IndexContentResolver];
 };
 
 export const getCasinaEntities = () => {
-  return [
-    ContactEntity
-  ] as any[];
+  return [ContactEntity, CasinaModuleEntity] as any[];
 };
 
 export const CASINA_MODULE = 'casina';
