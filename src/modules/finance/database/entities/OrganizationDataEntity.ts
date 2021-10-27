6946500eb71c7121602b6b6d4000e80365cd1bd4
Liana Julia <lianabeatriz93@gmail.com>
2021-10-27 12:38:52 -0400

    Add new fields in Organization data entity and service

--- src/modules/finance/database/entities/OrganizationDataEntity.ts
+++ src/modules/finance/database/entities/OrganizationDataEntity.ts
@@ -1,44 +1,36 @@
-import { Entity, Column, BaseEntity, PrimaryColumn } from 'typeorm'
+import { Entity, Column, BaseEntity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class OrganizationDataEntity extends BaseEntity {
-  @PrimaryColumn() id: number
-  @Column({ nullable: true, default: '' }) name?: string
-  @Column({ nullable: true, default: '' }) description?: string
-  @Column({ nullable: true, default: '' }) address?: string
-  @Column({ nullable: true, default: '' }) phone?: string
-  @Column({ nullable: true, default: '' }) email?: string
-  @Column({ nullable: true, default: 'h' }) accountingUnit?: string
-  @Column({ nullable: true, default: 'CHF' }) accountingCoin?: string
-  @Column({ nullable: true, default: 'FakeAccount' }) accountingLabel?: string
-
+  @PrimaryColumn() id: number;
+  @Column({ nullable: true, default: '' }) name?: string;
+  @Column({ nullable: true, default: '' }) description?: string;
+  @Column({ nullable: true, default: '' }) address?: string;
+  @Column({ nullable: true, default: '' }) place?: string;
+  @Column({ nullable: true, default: 0 }) cap?: number;
+  @Column({ nullable: true, default: '' }) phone?: string;
+  @Column({ nullable: true, default: '' }) email?: string;
+  @Column({ nullable: true, default: '' }) web?: string;
+  @Column({ nullable: true, default: 'h' }) accountingUnit?: string;
+  @Column({ nullable: true, default: 'CHF' }) accountingCoin?: string;
+  @Column({ nullable: true, default: 'FakeAccount' }) accountingLabel?: string;
   @Column({ type: 'float', nullable: true, default: 0 })
-  accountingInternetPrice?: number
+  accountingInternetPrice?: number;
+  @Column({ nullable: true, default: '' }) bankName?: string;
+  @Column({ nullable: true, default: '' }) bankBIC?: string;
+  @Column({ nullable: true, default: '' }) bankIBAN?: string;
+  @Column({ nullable: true, default: '' }) bankNo?: string;
+  @Column({ nullable: true, default: '' }) bankPlace?: string;
+  @Column({ nullable: true, default: '' }) bankAddress?: string;
+  @Column({ nullable: true, default: 30 }) bankValidDays?: number;
 
-  constructor(
-    numberId: number,
-    name?: string,
-    description?: string,
-    address?: string,
-    phone?: string,
-    email?: string,
-    accountingUnit?: string,
-    accountingCoin?: string,
-    accountingInternetPrice?: number,
-    accountingLabel?: string
-  ) {
-    super()
-    this.id = numberId
-    this.name = name
-    this.description = description
-    this.address = address
-    this.phone = phone
-    this.email = email
-    this.accountingUnit = accountingUnit
-    this.accountingCoin = accountingCoin
-    this.accountingInternetPrice = accountingInternetPrice
-    this.accountingLabel = accountingLabel
+  /**
+   *
+   */
+  constructor(id: number) {
+    super();
+    this.id = id;
   }
 }
 
-export default OrganizationDataEntity
+export default OrganizationDataEntity;
