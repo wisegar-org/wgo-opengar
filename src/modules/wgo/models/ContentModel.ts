5f1b5191475658ce40dda78ae526ba5919e01a03
Yariel Rodriguez <info@wisegar.org>
2021-11-05 09:32:08 -0400

    Fix translation register bug

--- src/modules/wgo/models/ContentModel.ts
+++ src/modules/wgo/models/ContentModel.ts
@@ -17,6 +17,9 @@ export class ContentModel {
     const itemsResult: ItemTranslationsResponseGQL[] = [];
     for (const item of arg.items) {
       const translation = await this.translationService.getTranslation(arg.languageId, item.key, item.trim);
+      if (translation === item.key) {
+        await this.translationService.setTranslation(arg.languageId, item.key, translation);
+      }
       itemsResult.push(<ItemTranslationsResponseGQL>{ key: item.key, value: translation });
     }
     return <GetListTranslationsResponseGQL>{ items: itemsResult };
