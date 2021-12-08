48c9e41cca752d93bfc0aa9554f5798fd648b445
Yariel Rodriguez <info@wisegar.org>
2021-12-08 10:21:06 -0500

    Fix issues details dialog

--- client/src/modules/finance/components/Issues/IssueDetailsDialog/IssueDetailsDialog.vue
+++ client/src/modules/finance/components/Issues/IssueDetailsDialog/IssueDetailsDialog.vue
@@ -7,8 +7,8 @@
     styleDialog="width: 500px; max-width: 80vw"
   >
     <template slot="content">
-      <AddIncomeEditor
-        :incomeToEdit="incomeToEdit"
+      <IssueDetails
+        :issue="issue"
         :close="() => onClose()"
         :showLoading="value => (showLoading = value)"
       />
@@ -20,16 +20,20 @@
 import { IssuesRecord } from '../../../models/models';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import IssueDetails from './IssueDetails.vue';
+import Dialog from '../../../../wgo/components/Dialog/Dialog.vue';
 
 @Component({
   components: {
-    IssueDetails
+    IssueDetails,
+    Dialog
   }
 })
 export default class IssueDetailsDialog extends Vue {
   @Prop({ default: false }) showModal!: boolean;
   @Prop() close!: () => void;
   @Prop() issue!: IssuesRecord;
+
+  showLoading = false;
 
   onClose() {
     if (this.close) {
