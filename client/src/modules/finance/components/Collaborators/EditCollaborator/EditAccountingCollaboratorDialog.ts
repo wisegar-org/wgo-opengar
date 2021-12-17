bbd85354db5a5e41bdcda0122720b76ba05ce433
Yariel Rodriguez <info@wisegar.org>
2021-12-17 09:12:12 -0500

    Fix Collaborator editor modal and view details component

---
+++ client/src/modules/finance/components/Collaborators/EditCollaborator/EditAccountingCollaboratorDialog.ts
@@ -0,0 +1,253 @@
+import { CollaboratorRecord } from '../../../models/models';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import Dialog from '../../../../wgo/components/Dialog/Dialog.vue';
+import { githubActions, githubNamespace } from '../../../store';
+import { ApiSettings } from '../../../settings/ApiSettings';
+import { INotify, UserLogged } from '../../../../wgo/models';
+import {
+  componentsActionsKeys,
+  componentsNamespace
+} from '../../../../wgo/store/ComponentsState';
+import { Action, Getter } from 'vuex-class';
+import {
+  ITranslationFinanceCollaboratorKeys,
+  WGO_FINANCE_COLLABORATOR_ROLE_CLIENT,
+  WGO_FINANCE_COLLABORATOR_ROLE_COLLABORATOR,
+  WGO_FINANCE_COLLABORATOR_ROLE_PROVIDER
+} from '../TranslationsKeys';
+import {
+  languageGetters,
+  languageNamespace
+} from 'src/modules/wgo/store/Language';
+import {
+  clientRoleType,
+  collaboratorRoleType,
+  providerRoleType
+} from '../ColumnsCollaborators';
+
+@Component({
+  components: {
+    Dialog
+  }
+})
+export default class EditAccountingCollaboratorDialog extends Vue {
+  @Prop({ default: false }) showModal!: boolean;
+  @Prop() close!: () => void;
+  @Prop() collaborator!: CollaboratorRecord;
+  @Getter(languageGetters.getTranslations, { namespace: languageNamespace })
+  translationContent!: ITranslationFinanceCollaboratorKeys;
+  @Action(githubActions.updateCollAccountingProps, {
+    namespace: githubNamespace
+  })
+  updateProperties!: (item: CollaboratorRecord) => Promise<boolean>;
+  @Action(githubActions.addClientProvider, {
+    namespace: githubNamespace
+  })
+  createClientProvider!: (item: CollaboratorRecord) => Promise<boolean>;
+  @Action(ApiSettings.USER_LOGGED_ACTION, {
+    namespace: ApiSettings.USER_NAMESPACE
+  })
+  loadLoggedUser!: () => Promise<void>;
+  @Getter(ApiSettings.USER_LOGGED_GETTER, {
+    namespace: ApiSettings.USER_NAMESPACE
+  })
+  loggedUser!: UserLogged;
+
+  @Action(componentsActionsKeys.notify, { namespace: componentsNamespace })
+  notify!: (value: INotify) => void;
+  showLoading = false;
+
+  editProps: {
+    name: string;
+    login: string;
+    card_number: string;
+    pay_by_hours: string;
+    // pay_to_internet: string;
+    email: string;
+    address: string;
+    cap: string;
+    place: string;
+    description: string;
+    type: string;
+  };
+
+  optionsRoles = [collaboratorRoleType, clientRoleType, providerRoleType];
+
+  selectedRoles: (string | { value: string; label?: string })[] = [];
+
+  constructor() {
+    super();
+    this.selectedRoles = [];
+    this.editProps = {
+      name: '',
+      login: '',
+      card_number: '',
+      email: '',
+      address: '',
+      pay_by_hours: '',
+      cap: '',
+      place: '',
+      // pay_to_internet: '',
+      description: '',
+      type: ''
+    };
+    this.updatePropsEditor();
+  }
+
+  @Watch('collaborator')
+  updatePropsEditor() {
+    this.editProps = this.collaborator
+      ? {
+          name: `${this.collaborator.name || this.collaborator.login}`,
+          login: `${this.collaborator.login}`,
+          card_number: `${this.collaborator.card_number || ''}`,
+          pay_by_hours: `${this.collaborator.pay_by_hours || ''}`,
+          // pay_to_internet: `${this.collaborator.pay_to_internet || ''}`,
+          email: `${this.collaborator.email || ''}`,
+          address: `${this.collaborator.address || ''}`,
+          cap: `${this.collaborator.cap || ''}`,
+          place: `${this.collaborator.place || ''}`,
+          description: `${this.collaborator.bio || ''}`,
+          type: `${this.collaborator.type}`
+        }
+      : {
+          name: '',
+          login: '',
+          card_number: '',
+          email: '',
+          address: '',
+          pay_by_hours: '',
+          cap: '',
+          place: '',
+          // pay_to_internet: '',
+          description: '',
+          type: ''
+        };
+
+    this.selectedRoles = this.editProps.type
+      .split(',')
+      .filter(role => this.isValidRole(role))
+      .map(item => ({
+        value: item,
+        label:
+          item in this.translationContent
+            ? (this.translationContent as any)[item]
+            : item
+      }));
+  }
+
+  @Watch('selectedRoles')
+  updateTypeRoles() {
+    this.editProps.type = (this.selectedRoles || [])
+      .map(item => (typeof item === 'string' ? item : item.value))
+      .join(',');
+  }
+
+  isValidRole(role: string) {
+    return (
+      role === WGO_FINANCE_COLLABORATOR_ROLE_PROVIDER ||
+      role === WGO_FINANCE_COLLABORATOR_ROLE_COLLABORATOR ||
+      role === WGO_FINANCE_COLLABORATOR_ROLE_CLIENT
+    );
+  }
+
+  isValid() {
+    return (
+      !!this.editProps.name &&
+      !!this.editProps.type &&
+      ((!!this.loggedUser && this.loggedUser.isSuperAdmin) ||
+        (!!this.editProps.card_number &&
+          !!this.editProps.pay_by_hours &&
+          !!this.editProps.email &&
+          !!this.editProps.cap &&
+          !!this.editProps.place &&
+          !!this.editProps.address))
+    );
+  }
+
+  isCollaborator() {
+    return (
+      this.editProps.type.indexOf(
+        WGO_FINANCE_COLLABORATOR_ROLE_COLLABORATOR
+      ) !== -1
+    );
+  }
+
+  @Watch('translationContent')
+  updateRolesLabel() {
+    collaboratorRoleType.label =
+      this.translationContent.WGO_FINANCE_COLLABORATOR_ROLE_COLLABORATOR ||
+      'Collaborator';
+    clientRoleType.label =
+      this.translationContent.WGO_FINANCE_COLLABORATOR_ROLE_CLIENT || 'Client';
+    providerRoleType.label =
+      this.translationContent.WGO_FINANCE_COLLABORATOR_ROLE_PROVIDER ||
+      'Provider';
+
+    this.selectedRoles = this.editProps.type.split(',').map(item => ({
+      value: item,
+      label: (this.translationContent as any)[item] || item
+    }));
+  }
+
+  async updateProps() {
+    this.showLoading = true;
+    const result = this.collaborator
+      ? await this.updateProperties(<CollaboratorRecord>{
+          id: this.collaborator.id,
+          name: this.editProps.name,
+          card_number: this.editProps.card_number,
+          pay_by_hours: parseFloat(this.editProps.pay_by_hours),
+          // pay_to_internet: parseFloat(this.editProps.pay_to_internet),
+          email: this.editProps.email,
+          bio: this.editProps.description,
+          address: this.editProps.address,
+          cap: this.editProps.cap,
+          place: this.editProps.place,
+          type: this.editProps.type
+        })
+      : await this.createClientProvider(<CollaboratorRecord>{
+          name: this.editProps.name,
+          login: this.editProps.name,
+          bio: this.editProps.description,
+          pay_by_hours: parseFloat(this.editProps.pay_by_hours),
+          email: this.editProps.email,
+          card_number: this.editProps.card_number,
+          address: this.editProps.address,
+          cap: this.editProps.cap,
+          place: this.editProps.place,
+          type: this.editProps.type
+        });
+
+    if (result) {
+      this.notify({
+        message: `Collaborator ${
+          this.collaborator ? 'updated' : 'created'
+        } successfully`,
+        type: 'positive'
+      });
+      if (
+        this.collaborator &&
+        this.loggedUser.userName === this.collaborator.login
+      ) {
+        await this.loadLoggedUser();
+      }
+      this.close();
+    }
+    this.showLoading = false;
+  }
+
+  onClose() {
+    if (this.close) {
+      this.close();
+    }
+  }
+
+  getTitle() {
+    return !this.collaborator
+      ? this.translationContent.WGO_FINANCE_COLLABORATOR_CREATE_TITLE ||
+          'Create contact'
+      : this.translationContent.WGO_FINANCE_COLLABORATOR_EDIT_TITLE ||
+          'Edit contact';
+  }
+}
--- client/src/modules/finance/components/Collaborators/EditCollaborator/EditAccountingCollaboratorDialog.vue
+++ client/src/modules/finance/components/Collaborators/EditCollaborator/EditAccountingCollaboratorDialog.vue
@@ -4,48 +4,121 @@
     :loading="showLoading"
     :title="getTitle()"
     :close="() => onClose()"
+    icon="contacts"
     styleDialog="width: 400px; max-width: 80vw"
   >
     <template slot="content">
-      <EditAccountingCollaborator
-        :close="close"
-        :collaborator="collaborator"
-        :showLoading="value => (showLoading = value)"
+      <!-- <q-input
+        v-if="!!collaborator && collaborator.isCollaborator"
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.login"
+        :autofocus="true"
+        label="User name"
+        dense
+        readonly
+      /> -->
+      <q-input
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.name"
+        :autofocus="true"
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_NAME"
+        dense
+      />
+      <q-input
+        v-if="!collaborator || isCollaborator()"
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.description"
+        autogrow
+        :autofocus="true"
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_DESCRIPTION"
+        dense
+      />
+      <q-input
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.email"
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_EMAIL"
+        dense
+      />
+      <q-input
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.address"
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_ADDRESS"
+        dense
+      />
+      <q-input
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        type="number"
+        v-model="editProps.cap"
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_CAP"
+        dense
+      />
+      <q-input
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.place"
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_PLACE"
+        dense
+      />
+      <q-select
+        square
+        outlined
+        dense
+        v-model="selectedRoles"
+        multiple
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_ROLE"
+        class="q-mx-md q-my-lg"
+        :options="optionsRoles"
+        option-value="value"
+        option-label="label"
+        clearable
+      />
+      <q-input
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        v-model="editProps.card_number"
+        mask="#### - #### - #### - ####"
+        dense
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_CARD_NUMBER"
+        :readonly="!loggedUser || !loggedUser.isSuperAdmin"
+      />
+      <q-input
+        v-if="isCollaborator()"
+        square
+        outlined
+        class="q-mx-md q-my-lg"
+        type="number"
+        v-model="editProps.pay_by_hours"
+        dense
+        :label="translationContent.WGO_FINANCE_COLLABORATOR_COLUMN_PAY_BY_HOURS"
+        :readonly="!loggedUser || !loggedUser.isSuperAdmin"
+      />
+    </template>
+    <template slot="buttons">
+      <q-btn
+        unelevated
+        @click="() => updateProps()"
+        color="primary"
+        align="center"
+        :disable="!isValid()"
+        class="col-12 col-sm-auto"
+        :label="translationContent.WGO_APPLY_BTN"
       />
     </template>
   </Dialog>
 </template>
 
-<script lang="ts">
-import { CollaboratorRecord } from '../../../models/models';
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import EditAccountingCollaborator from './EditAccountingCollaborator.vue';
-import Dialog from '../../../../wgo/components/Dialog/Dialog.vue';
-
-@Component({
-  components: {
-    EditAccountingCollaborator,
-    Dialog
-  }
-})
-export default class EditAccountingCollaboratorDialog extends Vue {
-  @Prop({ default: false }) showModal!: boolean;
-  @Prop() close!: () => void;
-  @Prop() collaborator!: CollaboratorRecord;
-  showLoading = false;
-
-  onClose() {
-    if (this.close) {
-      this.close();
-    }
-  }
-
-  getTitle() {
-    return !this.collaborator
-      ? 'Create client/provider'
-      : this.collaborator.isCollaborator
-      ? 'Edit collaborator'
-      : 'Edit client/provider';
-  }
-}
-</script>
+<script lang="ts" src="./EditAccountingCollaboratorDialog.ts" />
