3669e062a646cffa6019f1a5a0f02008a2ad0065
Yariel Rodriguez <info@wisegar.org>
2021-12-28 15:21:03 -0500

    Add Accounting confirm workflow translations

--- client/src/modules/finance/components/Accounting/AccountingConfirmDialog/AccountingConfirmDialog.vue
+++ client/src/modules/finance/components/Accounting/AccountingConfirmDialog/AccountingConfirmDialog.vue
@@ -1,44 +1,34 @@
 <template>
-  <Dialog
-    :showModal="showModal"
-    :loading="showLoading"
-    title="Confirm accounting"
-    :close="() => onClose()"
-    styleDialog="width: 500px; max-width: 80vw"
-  >
-    <template slot="content">
-      <q-card-section class="q-ma-none q-pa-none">
-        <AccountingDetails :accounting="accounting" />
-      </q-card-section>
-      <q-card-section
-        class="row q-ma-none items-center justify-center q-pa-none"
-      >
-        <q-btn
-          unelevated
-          @click="() => onClose()"
-          color="primary"
-          align="center"
-          class="col-12 col-sm-auto q-mt-sm q-mx-sm"
-          label="Close"
-        />
-        <q-btn
-          unelevated
-          @click="() => confirmAccounting()"
-          color="primary"
-          align="center"
-          class="col-12 col-sm-auto q-mt-sm q-mx-sm"
-          label="Confirm"
-        />
-      </q-card-section>
+  <wgo-dialog :options="options">
+    <q-card-section class="q-ma-none q-pa-none">
+      <AccountingDetails :accounting="accounting" />
+    </q-card-section>
+    <template slot="buttons">
+      <q-btn
+        unelevated
+        @click="() => onClose()"
+        color="primary"
+        align="center"
+        class="col-12 col-sm-auto q-mt-sm q-mx-sm"
+        :label="translationContent.WGO_CLOSE_BTN"
+      />
+      <q-btn
+        unelevated
+        @click="() => confirmAccounting()"
+        color="primary"
+        align="center"
+        class="col-12 col-sm-auto q-mt-sm q-mx-sm"
+        :label="translationContent.WGO_CONFIRM_BTN"
+      />
     </template>
-  </Dialog>
+  </wgo-dialog>
 </template>
 
 <script lang="ts">
-import { Action } from 'vuex-class';
+import { Action, Getter } from 'vuex-class';
 import { AccountRecord } from '../../../models/models';
 import { githubActions, githubNamespace } from '../../../store';
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import AccountingDetails from '../AccountingDetailsDialog/AccountingDetails.vue';
 import Dialog from '../../../../wgo/components/Dialog/Dialog.vue';
 import {
@@ -46,6 +36,16 @@ import {
   componentsNamespace
 } from '../../../../wgo/store/ComponentsState';
 import { INotify } from '../../../../wgo/models';
+import { IWGODialogOptions } from '@wisegar-org/quasar-app-extension-wgo-vue-components/src/lib';
+import {
+  languageActions,
+  languageGetters,
+  languageNamespace
+} from '../../../../wgo/store/Language';
+import {
+  ITranslationFinanceAccountingKeys,
+  TranslationsKeys
+} from '../TranslationsKeys';
 
 @Component({
   components: {
@@ -54,6 +54,12 @@ import { INotify } from '../../../../wgo/models';
   }
 })
 export default class AccountingConfirmDialog extends Vue {
+  @Action(languageActions.registerTranslations, {
+    namespace: languageNamespace
+  })
+  registerTranslations!: (data: unknown) => Promise<boolean>;
+  @Getter(languageGetters.getTranslations, { namespace: languageNamespace })
+  translationContent!: ITranslationFinanceAccountingKeys;
   @Prop({ default: false }) showModal!: boolean;
   @Prop() close!: () => void;
   @Prop() accounting!: AccountRecord;
@@ -64,14 +70,27 @@ export default class AccountingConfirmDialog extends Vue {
 
   showLoading = false;
 
+  options: IWGODialogOptions = {
+    open: this.showModal,
+    onClose: () => this.onClose(),
+    hideButtons: true,
+    fullHeight: true,
+    fullWidth: true
+  };
+
   async confirmAccounting() {
     this.showLoading = true;
     if (await this.confirmAccountingAction(this.accounting.id)) {
       this.notify({
-        message: 'Accounting confirmed successfully ',
+        message: this.translationContent.WGO_FINANCE_ACCOUNTING_CONFIRM_SUCCESS,
         type: 'positive'
       });
       this.close();
+    } else {
+      this.notify({
+        message: this.translationContent.WGO_FINANCE_ACCOUNTING_CONFIRM_FAIL,
+        type: 'negative'
+      });
     }
     this.showLoading = false;
   }
@@ -80,6 +99,21 @@ export default class AccountingConfirmDialog extends Vue {
     if (this.close) {
       this.close();
     }
+  }
+
+  @Watch('showModal')
+  showModalChange() {
+    this.options.open = this.showModal;
+  }
+
+  @Watch('translationContent')
+  translationsChange() {
+    this.options.title = this.translationContent.WGO_FINANCE_ACCOUNTING_CONFIRM_TITLE;
+  }
+
+  async mounted() {
+    await this.registerTranslations(TranslationsKeys);
+    this.options.title = this.translationContent.WGO_FINANCE_ACCOUNTING_CONFIRM_TITLE;
   }
 }
 </script>
