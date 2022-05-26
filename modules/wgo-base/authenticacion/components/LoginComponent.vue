0e8fe62e4da44fca3a427df61ecc23bf474a44c8
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2022-05-27 00:09:36 +0200

    Update wgo-base

---
+++ modules/wgo-base/authenticacion/components/LoginComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-h3">LoginComponent</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
--- modules/wgo-module-template/client/src/pages/IndexPage.vue
+++ modules/wgo-module-template/client/src/pages/IndexPage.vue
@@ -1,49 +1,53 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div class="col-12">
+      <example-component
+        title="Example component template"
+        active
+        :todos="todos"
+        :meta="meta"
+      ></example-component>
+    </div>
+    <div class="col-12"><DummyComponent /></div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
+import { Todo, Meta } from "components/models";
+import ExampleComponent from "components/ExampleComponent.vue";
+import { defineComponent, ref } from "vue";
+import DummyComponent from "../../../../wgo-base/components/DummyComponent.vue";
 
 export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
+  name: "IndexPage",
+  components: { ExampleComponent, DummyComponent },
   setup() {
     const todos = ref<Todo[]>([
       {
         id: 1,
-        content: 'ct1'
+        content: "ct1",
       },
       {
         id: 2,
-        content: 'ct2'
+        content: "ct2",
       },
       {
         id: 3,
-        content: 'ct3'
+        content: "ct3",
       },
       {
         id: 4,
-        content: 'ct4'
+        content: "ct4",
       },
       {
         id: 5,
-        content: 'ct5'
-      }
+        content: "ct5",
+      },
     ]);
     const meta = ref<Meta>({
-      totalCount: 1200
+      totalCount: 1200,
     });
     return { todos, meta };
-  }
+  },
 });
 </script>
