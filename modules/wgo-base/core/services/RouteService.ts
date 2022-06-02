d39f3fede3920d13b3268f531a0ef071c34a7237
Yariel  Rodriguez <info@wisegar.org>
2022-06-02 16:11:57 -0400

    Add ApiService and RouteService on wgo-base core services

---
+++ modules/wgo-base/core/services/RouteService.ts
@@ -0,0 +1,45 @@
+import Router, { useRouter } from "vue-router";
+
+export interface IRouteService {
+  goTo(path: string, query?: any): void;
+  goToPathName(path: string, query?: any): void;
+  goBack(): void;
+  getRouteUrlPath(): void;
+}
+
+export class RouteService implements IRouteService {
+  private frameworkRouter: Router.Router;
+  constructor(frameworkRouter: Router.Router) {
+    this.frameworkRouter = frameworkRouter;
+  }
+
+  public goTo(path: string, query?: any) {
+    if (this.frameworkRouter.currentRoute.value.path !== path) {
+      if (query) {
+        this.frameworkRouter
+          .push({ path, query })
+          .catch((err) => console.log(err));
+        return;
+      }
+      this.frameworkRouter.push(path).catch((err) => console.log(err));
+    } else {
+      this.frameworkRouter.go(0);
+    }
+  }
+
+  public goToPathName(name: string, query?: any) {
+    this.frameworkRouter.push({
+      name: name,
+      query: query,
+      params: {},
+    });
+  }
+
+  public goBack() {
+    this.frameworkRouter.back();
+  }
+
+  public getRouteUrlPath() {
+    this.frameworkRouter.currentRoute.value.fullPath;
+  }
+}
