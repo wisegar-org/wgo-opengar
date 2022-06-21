1a50eac8527e8cce0661ade7e0cb0eeb148b4fc5
Yariel  Rodriguez <info@wisegar.org>
2022-06-20 22:04:19 -0400

    Fix load me user on mounted function

--- modules/wgo-swisspay/client/src/App.vue
+++ modules/wgo-swisspay/client/src/App.vue
@@ -3,17 +3,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { useNotifyStore, INotify } from "./stores/notifyStore";
-import { QNotifyCreateOptions, useQuasar } from "quasar";
-import { AuthService } from "../../../wgo-base/authenticacion/services/AuthService";
-import { useAuthStore } from "./stores/authStore";
-import { USER_AUTH_TOKEN } from "../../../wgo-base/authenticacion/models";
-import HelloWorld from "../../../wgo-base-lib/src/components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import { useNotifyStore, INotify } from './stores/notifyStore';
+import { QNotifyCreateOptions, useQuasar } from 'quasar';
+import { AuthService } from '../../../wgo-base/authenticacion/services/AuthService';
+import { useAuthStore } from './stores/authStore';
+import { USER_AUTH_TOKEN } from '../../../wgo-base/authenticacion/models';
+import HelloWorld from '../../../wgo-base-lib/src/components/HelloWorld.vue';
 
 export default defineComponent({
   components: { HelloWorld },
-  name: "App",
+  name: 'App',
   setup() {
     const notifyStore = useNotifyStore();
     const quasar = useQuasar();
@@ -23,10 +23,18 @@ export default defineComponent({
     const authStore = useAuthStore();
     const authService = new AuthService();
     const token = localStorage.getItem(USER_AUTH_TOKEN);
-    if (!!token) {
-      authService.me({ token }).then((user) => {
-        if (!!user) authStore.setLogin({ token, user } as any);
-      });
+
+    return {
+      token,
+      authStore,
+      authService,
+    };
+  },
+  async mounted() {
+    if (!this.token) return;
+    const user = await this.authService.me({ token: this.token || '' });
+    if (!!user) {
+      this.authStore.setLogin({ token: this.token, user } as any);
     }
   },
 });
