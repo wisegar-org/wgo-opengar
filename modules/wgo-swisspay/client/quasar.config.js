db466d93c2685de6dddc5e7a01ae870f3546b3b9
Yariel  Rodriguez <info@wisegar.org>
2022-06-27 16:21:04 -0400

    Add store boot to initialize stores

--- modules/wgo-swisspay/client/quasar.config.js
+++ modules/wgo-swisspay/client/quasar.config.js
@@ -25,7 +25,7 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
-    boot: ['i18n', 'axios'],
+    boot: ['i18n', 'axios', 'store'],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
     css: ['app.css'],
