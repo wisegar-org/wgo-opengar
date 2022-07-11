7fb7fd87cf41c827be171994cdab63cd039387d0
Yariel  Rodriguez <info@wisegar.org>
2022-07-11 12:36:08 -0400

    Add auth translations to settings

--- modules/wgo-swisspay/client/src/settings/translations.ts
+++ modules/wgo-swisspay/client/src/settings/translations.ts
@@ -2,6 +2,7 @@
 import { translations as transCore } from '../../../../wgo-base/core/models';
 import { translations as transTranslations } from '../../../../wgo-base/translation/models/translations';
 import { translations as languageTranslations } from '../../../../wgo-base/language/models/translations';
+import { translations as authTranslations } from '../../../../wgo-base/authentication/models/translations';
 
 //Project translation components
 import { translations as emailMediaTranslations } from '../components/EmailMedia/translations';
@@ -9,5 +10,6 @@ import { translations as emailMediaTranslations } from '../components/EmailMedia
 const tanslations: string[] = Object.values(transCore)
   .concat(Object.values(transTranslations))
   .concat(Object.values(languageTranslations))
+  .concat(Object.values(authTranslations))
   .concat(Object.values(emailMediaTranslations));
 export const Translations = tanslations;
