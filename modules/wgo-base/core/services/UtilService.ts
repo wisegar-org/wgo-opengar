2189f5712dcdd7265edaf373516e33800bfe603a
Yariel  Rodriguez <info@wisegar.org>
2022-07-12 00:19:42 -0400

    Add function to load streams to Util service

--- modules/wgo-base/core/services/UtilService.ts
+++ modules/wgo-base/core/services/UtilService.ts
@@ -5,4 +5,19 @@ export const UtilService = {
     if (!date) return "";
     return dayjs(date).format(format);
   },
+  async readStreamData(stream: any) {
+    return new Promise((res, rej) => {
+      let dataStm: any[] = [];
+      stream
+        .on("error", () => {
+          rej(null);
+        })
+        .on("data", (data: any) => {
+          dataStm.push(data);
+        })
+        .on("end", () => {
+          res(Buffer.concat(dataStm));
+        });
+    });
+  },
 };
