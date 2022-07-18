1d5541ca7ab155257291eef8366a011ab8c1e498
Yariel  Rodriguez <info@wisegar.org>
2022-07-18 12:42:04 -0400

    Fix app layout style

--- modules/wgo-swisspay/client/src/layouts/MainLayout.vue
+++ modules/wgo-swisspay/client/src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="showMenu" />
 
