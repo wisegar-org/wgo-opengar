2b236a35475c36257206f24ca6085fc745182d1f
Liana Julia <lianabeatriz93@gmail.com>
2022-07-21 15:26:20 -0400

    Add RegisterEmployee page

---
+++ modules/wgo-swisspay/client/src/pages/Employees/RegisterEmployeePage.vue
@@ -0,0 +1,91 @@
+<template>
+  <RegisterComponent
+    @goBack="onGoBack"
+    :tranStore="tranStore"
+    :roles="roles"
+    :isEmailConfirmed="isEmailConfirmed"
+    @onRegister="onRegister"
+    @onHome="onGoHome"
+  />
+</template>
+
+<script lang="ts">
+import { useNotifyStore } from '../../stores/notifyStore';
+import { defineComponent } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { AuthPaths } from '../../../../../wgo-base/authentication/router';
+import { BaseTranslateComponent } from '../../../../../wgo-base/core/components/BaseComponents';
+import { IUser } from '../../../../../wgo-base/core/models';
+import { RouteService } from '../../../../../wgo-base/core/services/RouteService';
+import { TranslationStore } from '../../../../../wgo-base/translation/models/TranslationStore';
+import { USER_ROLE } from '../../../../src/models/constants';
+import { Paths } from '../../router/paths';
+import { EmployeesService } from '../../services/Employees/EmployeesService';
+import { useTranslationStore } from '../../stores/translationStore';
+import { translations } from '../../components/Employees/translations';
+import RegisterComponent from '../../../../../wgo-base/authentication/components/RegisterComponent.vue';
+
+export default defineComponent({
+  components: {
+    RegisterComponent,
+  },
+  data() {
+    const route = useRoute();
+    const router = useRouter();
+    const roles = [USER_ROLE];
+    return {
+      roles,
+      isEmailConfirmed: false,
+      enterprise_id: 0,
+      routeService: new RouteService(router),
+      token: (route.query.token as string) || '',
+    };
+  },
+  setup() {
+    const translationStore = useTranslationStore();
+    const notyfyStore = useNotifyStore();
+    const employeesService = new EmployeesService();
+    const { getLabel } = new BaseTranslateComponent();
+
+    return {
+      employeesService,
+      notyfyStore,
+      tranStore: translationStore.translationStore as TranslationStore,
+      getLabel: (name: string) => getLabel(translationStore.translationStore as TranslationStore, name),
+    };
+  },
+  methods: {
+    onGoHome() {
+      this.routeService.goTo(Paths.home.path);
+    },
+    onGoBack() {
+      this.routeService.goBack();
+    },
+    async onRegister(user: IUser) {
+      if (this.token && this.enterprise_id) {
+        await this.employeesService.addEmployee(user.email, user.name, this.enterprise_id, user.id);
+      }
+      if (this.isEmailConfirmed) this.routeService.goTo(AuthPaths.authLogin.path);
+      else
+        this.routeService.goTo(AuthPaths.authEmailSended.path, {
+          email: user.email,
+        });
+    },
+  },
+  async mounted() {
+    if (this.token) {
+      const resp = await this.employeesService.checkEmployeeToken(this.token);
+      if (resp) {
+        this.enterprise_id = resp;
+      } else {
+        this.onGoHome();
+        this.notyfyStore.setNotify({
+          position: 'top',
+          type: 'negative',
+          message: this.getLabel(translations.INVALID_TOKEN_EMPLOYEE),
+        });
+      }
+    }
+  },
+});
+</script>
