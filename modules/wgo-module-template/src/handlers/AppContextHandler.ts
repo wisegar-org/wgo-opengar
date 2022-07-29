1056bba64e42206f1c97b77e9ab8b4be412bcc6d
Liana Julia <lianabeatriz93@gmail.com>
2022-07-28 20:54:03 -0400

    Add superadmin auth context

--- modules/wgo-module-template/src/handlers/AppContextHandler.ts
+++ modules/wgo-module-template/src/handlers/AppContextHandler.ts
@@ -1,12 +1,40 @@
 import { IContextOptions } from "@wisegar-org/wgo-server";
+import {
+  GetEmailAppAddressKey,
+  GetExpiresInKey,
+  GetHostBaseKey,
+  GetPrivateKey,
+  GetPublicKey,
+} from "@wisegar-org/wgo-settings";
+import { SUPERADMIN } from "../wgo-base/authentication/models";
+import { translations } from "../wgo-base/core/models";
+import { UserRolesModel } from "../wgo-base/authentication/models/UserRolesModel";
+import { PostgresDataSource } from "../dataSources";
+import { IContext } from "../models";
+
+const authModel = new UserRolesModel({
+  privateKey: GetPrivateKey(),
+  publicKey: GetPublicKey(),
+  hostBase: GetHostBaseKey(),
+  dataSource: PostgresDataSource,
+  tokenExpiresIn: GetExpiresInKey(),
+  tokenRegisterExpiresIn: "24h",
+  emailOptions: { from: GetEmailAppAddressKey() } as any,
+  transportEmailOptions: {},
+});
 
 export const AppContextHandler = async (options: IContextOptions) => {
   if (!options) {
-    throw new Error("Invalid params");
+    throw new Error(translations.INVALID_PARAMS);
   }
   const { tokenPayload, requestHeaders } = options;
-  const ctx: any = {};
+  const ctx = {} as IContext;
   if (!tokenPayload) return ctx;
+  const user = await authModel.getUser(parseInt(tokenPayload.userId));
+  if (user) {
+    ctx.user = user;
+    ctx.user.isSuperAdmin = user.roles.indexOf(SUPERADMIN) !== -1;
+  }
   // TODO: Add context definition here
-  return ctx;
+  return ctx as any;
 };
