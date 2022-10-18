428a9f82f3b1c9e76a3f93923633de26e7ca0a9f
Yariel Rodriguez <yariel.re@gmail.com>
2022-10-18 10:18:59 +0200

    Update version on index file

--- modules/wgo-cli/src/index.ts
+++ modules/wgo-cli/src/index.ts
@@ -3,7 +3,7 @@ import { EmptyVersion } from "./options/ICmdOptions";
 import { Logger } from "./utils/Logger";
 
 export const wgoCli = () => {
-  const current_version: string = "0.0.1-build.0";
+  const current_version: string = "0.0.2-2";
 
   const processVersion = process.env.npm_package_version
     ? process.env.npm_package_version
