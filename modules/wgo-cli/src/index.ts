054ed6d65ec72c6ddd818bd9848834e42ded8f57
Yariel Rodriguez <yariel.re@gmail.com>
2022-10-18 15:19:21 +0200

    Update prerelease cli

--- modules/wgo-cli/src/index.ts
+++ modules/wgo-cli/src/index.ts
@@ -3,7 +3,7 @@ import { EmptyVersion } from "./options/ICmdOptions";
 import { Logger } from "./utils/Logger";
 
 export const wgoCli = () => {
-  const current_version: string = "0.0.2-12";
+  const current_version: string = "0.0.2-13";
 
   const processVersion = process.env.npm_package_version
     ? process.env.npm_package_version
