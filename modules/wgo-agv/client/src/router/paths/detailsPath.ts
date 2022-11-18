6976ad6e31669e05ccb8fa146a691c2d91859297
Liana Juliá <lianabeatriz93@gmail.com>
2022-11-18 15:20:52 -0500

    Add new mothels and add EventService and queries

---
+++ modules/wgo-agv/client/src/router/paths/detailsPath.ts
@@ -0,0 +1,19 @@
+import { IRouteObject } from "src/wgo-base/core/models";
+
+export const AGVDetailsPaths: IRouteObject = {
+  corsiDetails: {
+    path: "/corsi/details",
+    name: "agv_corsi_details",
+    label: "WGO_CORSI_DETAILS",
+  },
+  eventiDetails: {
+    path: "/eventi/details",
+    name: "agv_eventi_details",
+    label: "WGO_EVENTI_DETAILS",
+  },
+  newsInscription: {
+    path: "/subscription",
+    name: "agv_new_iscrizione",
+    label: "WGO_NEW_INSCRIPTION",
+  },
+};
