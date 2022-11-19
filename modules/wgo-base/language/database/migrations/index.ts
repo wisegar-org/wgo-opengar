100025980ff2ffa9b660f190e377d491a72b6add
Liana Juliá <lianabeatriz93@gmail.com>
2022-11-19 14:42:27 -0500

    Add get migrations methods on wgo-base modules

---
+++ modules/wgo-base/language/database/migrations/index.ts
@@ -0,0 +1,6 @@
+import { addLanguageEntity1656012423678 } from "./1656012423678-addLanguageEntity";
+import { addLanguageHistoric1668550415911 } from "./1668550415911-addLanguageHistoric";
+
+export const getLanguageMigrations = () => {
+  return [addLanguageEntity1656012423678, addLanguageHistoric1668550415911];
+};
