91e6f08da3be469dd573e940c6e56ad194978f0f
Liana Juliá <lianabeatriz93@gmail.com>
2022-11-19 15:23:48 -0500

    Fix wgo-casina migrations import and update package jsons

--- modules/wgo-casina/src/dataSources.ts
+++ modules/wgo-casina/src/dataSources.ts
@@ -19,19 +19,25 @@ import { ContactMeEntity } from "./wgo-base/contact/database/entities/ContactMeE
 import { CasinaModuleEntity } from "./database/entities/CasinaModuleEntity";
 
 /** Migrations */
-import { migrations1651766519693 } from "./wgo-base/authentication/database/migrations/1651766519693-migrations";
-import { addRoleEntity1656005738186 } from "./wgo-base/authentication/database/migrations/1656005738186-addRoleEntity";
-import { addLanguageEntity1656012423678 } from "./wgo-base/language/database/migrations/1656012423678-addLanguageEntity";
-import { addCertificateFields1656434163078 } from "./wgo-base/authentication/database/migrations/1656434163078-addCertificateFields";
-import { createTranslationEntity1656439638048 } from "./wgo-base/translation/database/migrations/1656439638048-createTranslationEntity";
-import { addSettingsEntity1658164004212 } from "./wgo-base/settings/database/migrations/1658164004212-addSettingsEntity";
-import { renameTypeSettings1658240270133 } from "./wgo-base/settings/database/migrations/1658240270133-renameTypeSettings";
-import { removeAppSettings1658240565587 } from "./wgo-base/settings/database/migrations/1658240565587-removeAppSettings";
-import { fixAdminPassword1658417129112 } from "./wgo-base/authentication/database/migrations/1658417129112-fixAdminPassword";
-import { addMediaEntity1658931161380 } from "./wgo-base/storage/database/migrations/1658931161380-addMediaEntity";
-import { addStorageEntity1658931419178 } from "./wgo-base/storage/database/migrations/1658931419178-addStorageEntity";
-import { addContactMeEntity1658932057343 } from "./wgo-base/contact/database/migrations/1658932057343-addContactMeEntity";
-import { addCasinaIndexContentDatabase1659037006696 } from "./database/migrations/1659037006696-addCasinaIndexContentDatabase";
+import { getAuthenticationMigrations } from "./wgo-base/authentication/database/migrations";
+import { getContactMigrations } from "./wgo-base/contact/database/migrations";
+import { getHistoricMigrations } from "./wgo-base/historic/database/migrations";
+import { getLanguageMigrations } from "./wgo-base/language/database/migrations";
+import { getSettingsMigrations } from "./wgo-base/settings/database/migrations";
+import { getStorageMigrations } from "./wgo-base/storage/database/migrations";
+import { getTemplateMigrations } from "./wgo-base/template/database/migrations";
+import { getTranslationMigrations } from "./wgo-base/translation/database/migrations";
+import { getCasinaMigrations } from "./database/migrations";
+
+const migrations = getAuthenticationMigrations()
+  .concat(getContactMigrations())
+  .concat(getHistoricMigrations())
+  .concat(getLanguageMigrations())
+  .concat(getSettingsMigrations())
+  .concat(getStorageMigrations())
+  .concat(getTemplateMigrations())
+  .concat(getTranslationMigrations())
+  .concat(getCasinaMigrations());
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
@@ -53,21 +59,7 @@ export const dataSourceOptions: DataSourceOptions = {
     ContactMeEntity,
     CasinaModuleEntity,
   ],
-  migrations: [
-    migrations1651766519693,
-    addRoleEntity1656005738186,
-    addLanguageEntity1656012423678,
-    addCertificateFields1656434163078,
-    createTranslationEntity1656439638048,
-    addSettingsEntity1658164004212,
-    renameTypeSettings1658240270133,
-    removeAppSettings1658240565587,
-    fixAdminPassword1658417129112,
-    addMediaEntity1658931161380,
-    addStorageEntity1658931419178,
-    addContactMeEntity1658932057343,
-    addCasinaIndexContentDatabase1659037006696,
-  ],
+  migrations: migrations,
   subscribers: [],
 };
 
---
+++ modules/wgo-casina/src/database/migrations/index.ts
@@ -0,0 +1,5 @@
+import { addCasinaIndexContentDatabase1659037006696 } from "./1659037006696-addCasinaIndexContentDatabase";
+
+export const getCasinaMigrations = () => {
+  return [addCasinaIndexContentDatabase1659037006696];
+};
