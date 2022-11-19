70f15bc20eacaedee1bd588fcd1f95f02b27c618
Liana Juliá <lianabeatriz93@gmail.com>
2022-11-19 15:32:14 -0500

    Fix wgo-wisegar migrations import and update package jsons

--- modules/wgo-wisegar/src/dataSources.ts
+++ modules/wgo-wisegar/src/dataSources.ts
@@ -30,20 +30,25 @@ import { ProjectEntity } from "./database/entities/Finance/ProjectEntity";
 import { RepositoryEntity } from "./database/entities/Finance/RepositoryEntity";
 
 /********************************* Migrations **********************************/
-import { migrations1651766519693 } from "./wgo-base/authentication/database/migrations/1651766519693-migrations";
-import { addRoleEntity1656005738186 } from "./wgo-base/authentication/database/migrations/1656005738186-addRoleEntity";
-import { addLanguageEntity1656012423678 } from "./wgo-base/language/database/migrations/1656012423678-addLanguageEntity";
-import { addCertificateFields1656434163078 } from "./wgo-base/authentication/database/migrations/1656434163078-addCertificateFields";
-import { createTranslationEntity1656439638048 } from "./wgo-base/translation/database/migrations/1656439638048-createTranslationEntity";
-import { addSettingsEntity1658164004212 } from "./wgo-base/settings/database/migrations/1658164004212-addSettingsEntity";
-import { renameTypeSettings1658240270133 } from "./wgo-base/settings/database/migrations/1658240270133-renameTypeSettings";
-import { removeAppSettings1658240565587 } from "./wgo-base/settings/database/migrations/1658240565587-removeAppSettings";
-import { fixAdminPassword1658417129112 } from "./wgo-base/authentication/database/migrations/1658417129112-fixAdminPassword";
-import { addContactMeEntity1658932057343 } from "./wgo-base/contact/database/migrations/1658932057343-addContactMeEntity";
-import { addMediaEntity1658931161380 } from "./wgo-base/storage/database/migrations/1658931161380-addMediaEntity";
-import { createIndexContentTb1663686346323 } from "./database/migrations/1663686346323-createIndexContentTb";
-import { addStorageEntity1658931419178 } from "./wgo-base/storage/database/migrations/1658931419178-addStorageEntity";
-import { createGithubFinanceDatabase1663797822990 } from "./database/migrations/Finance/1663797822990-createGithubFinanceDatabase";
+import { getAuthenticationMigrations } from "./wgo-base/authentication/database/migrations";
+import { getContactMigrations } from "./wgo-base/contact/database/migrations";
+import { getHistoricMigrations } from "./wgo-base/historic/database/migrations";
+import { getLanguageMigrations } from "./wgo-base/language/database/migrations";
+import { getSettingsMigrations } from "./wgo-base/settings/database/migrations";
+import { getStorageMigrations } from "./wgo-base/storage/database/migrations";
+import { getTemplateMigrations } from "./wgo-base/template/database/migrations";
+import { getTranslationMigrations } from "./wgo-base/translation/database/migrations";
+import { getWisegarMigrations } from "./database/migrations";
+
+const migrations = getAuthenticationMigrations()
+  .concat(getContactMigrations())
+  .concat(getHistoricMigrations())
+  .concat(getLanguageMigrations())
+  .concat(getSettingsMigrations())
+  .concat(getStorageMigrations())
+  .concat(getTemplateMigrations())
+  .concat(getTranslationMigrations())
+  .concat(getWisegarMigrations());
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
@@ -71,22 +76,7 @@ export const dataSourceOptions: DataSourceOptions = {
     RepositoryEntity,
     AccountEntity,
   ],
-  migrations: [
-    migrations1651766519693,
-    addRoleEntity1656005738186,
-    addLanguageEntity1656012423678,
-    addCertificateFields1656434163078,
-    createTranslationEntity1656439638048,
-    addSettingsEntity1658164004212,
-    renameTypeSettings1658240270133,
-    removeAppSettings1658240565587,
-    fixAdminPassword1658417129112,
-    addContactMeEntity1658932057343,
-    addMediaEntity1658931161380,
-    createIndexContentTb1663686346323,
-    addStorageEntity1658931419178,
-    createGithubFinanceDatabase1663797822990,
-  ],
+  migrations: migrations,
   subscribers: [],
 };
 
--- modules/wgo-wisegar/src/database/migrations/1663686346323-createIndexContentTb.ts
+++ modules/wgo-wisegar/src/database/migrations/1663686346323-createIndexContentTb.ts
@@ -1,22 +1,21 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createIndexContentTb1663686346323 implements MigrationInterface {
-    name = 'createIndexContentTb1663686346323'
+  name = "createIndexContentTb1663686346323";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "index_content_entity" ("id" SERIAL NOT NULL, "imageId" integer, CONSTRAINT "PK_8eea73d1f17017a43ac22a6d4a8" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "code" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "code" SET DEFAULT ' '`);
-        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "userName-unique" UNIQUE ("userName", "code")`);
-        await queryRunner.query(`ALTER TABLE "index_content_entity" ADD CONSTRAINT "FK_7a1255ae01ce33ffafa866f46e1" FOREIGN KEY ("imageId") REFERENCES "media"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "index_content_entity" ("id" SERIAL NOT NULL, "imageId" integer, CONSTRAINT "PK_8eea73d1f17017a43ac22a6d4a8" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "index_content_entity" ADD CONSTRAINT "FK_7a1255ae01ce33ffafa866f46e1" FOREIGN KEY ("imageId") REFERENCES "media"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "index_content_entity" DROP CONSTRAINT "FK_7a1255ae01ce33ffafa866f46e1"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "userName-unique"`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "code" SET DEFAULT ''`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "code" SET NOT NULL`);
-        await queryRunner.query(`DROP TABLE "index_content_entity"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "index_content_entity" DROP CONSTRAINT "FK_7a1255ae01ce33ffafa866f46e1"`
+    );
+    await queryRunner.query(`DROP TABLE "index_content_entity"`);
+  }
 }
