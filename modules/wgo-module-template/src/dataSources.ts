0e661fa37f761d47c149ae7244bac223362d3ab6
Liana Juliá <lianabeatriz93@gmail.com>
2022-11-19 15:45:51 -0500

    Fix wgo-module-template migrations import and update package jsons

--- modules/wgo-module-template/src/dataSources.ts
+++ modules/wgo-module-template/src/dataSources.ts
@@ -16,16 +16,23 @@ import { SettingsEntity } from "./wgo-base/settings/database/entities/SettingsEn
 import { ContactMeEntity } from "./wgo-base/contact/database/entities/ContactMeEntity";
 
 /** Migrations */
-import { migrations1651766519693 } from "./wgo-base/authentication/database/migrations/1651766519693-migrations";
-import { addRoleEntity1656005738186 } from "./wgo-base/authentication/database/migrations/1656005738186-addRoleEntity";
-import { addLanguageEntity1656012423678 } from "./wgo-base/language/database/migrations/1656012423678-addLanguageEntity";
-import { addCertificateFields1656434163078 } from "./wgo-base/authentication/database/migrations/1656434163078-addCertificateFields";
-import { createTranslationEntity1656439638048 } from "./wgo-base/translation/database/migrations/1656439638048-createTranslationEntity";
-import { addSettingsEntity1658164004212 } from "./wgo-base/settings/database/migrations/1658164004212-addSettingsEntity";
-import { renameTypeSettings1658240270133 } from "./wgo-base/settings/database/migrations/1658240270133-renameTypeSettings";
-import { removeAppSettings1658240565587 } from "./wgo-base/settings/database/migrations/1658240565587-removeAppSettings";
-import { fixAdminPassword1658417129112 } from "./wgo-base/authentication/database/migrations/1658417129112-fixAdminPassword";
-import { addContactMeEntity1658932057343 } from "./wgo-base/contact/database/migrations/1658932057343-addContactMeEntity";
+import { getAuthenticationMigrations } from "./wgo-base/authentication/database/migrations";
+import { getContactMigrations } from "./wgo-base/contact/database/migrations";
+import { getHistoricMigrations } from "./wgo-base/historic/database/migrations";
+import { getLanguageMigrations } from "./wgo-base/language/database/migrations";
+import { getSettingsMigrations } from "./wgo-base/settings/database/migrations";
+import { getStorageMigrations } from "./wgo-base/storage/database/migrations";
+import { getTemplateMigrations } from "./wgo-base/template/database/migrations";
+import { getTranslationMigrations } from "./wgo-base/translation/database/migrations";
+
+const migrations = getAuthenticationMigrations()
+  .concat(getContactMigrations())
+  .concat(getHistoricMigrations())
+  .concat(getLanguageMigrations())
+  .concat(getSettingsMigrations())
+  .concat(getStorageMigrations())
+  .concat(getTemplateMigrations())
+  .concat(getTranslationMigrations());
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
@@ -44,18 +51,7 @@ export const dataSourceOptions: DataSourceOptions = {
     SettingsEntity,
     ContactMeEntity,
   ],
-  migrations: [
-    migrations1651766519693,
-    addRoleEntity1656005738186,
-    addLanguageEntity1656012423678,
-    addCertificateFields1656434163078,
-    createTranslationEntity1656439638048,
-    addSettingsEntity1658164004212,
-    renameTypeSettings1658240270133,
-    removeAppSettings1658240565587,
-    fixAdminPassword1658417129112,
-    addContactMeEntity1658932057343,
-  ],
+  migrations: migrations,
   subscribers: [],
 };
 
