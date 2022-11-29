1babbbfd5acdcdf0579f18ca09988a7dc75ca174
Liana Juliá <lianabeatriz93@gmail.com>
2022-11-29 15:38:39 -0500

    Add Newsletter Inscription page

--- modules/wgo-agv/client/src/pages/AdminAgv/AdminNewsletterInscriptionsPage.vue
+++ modules/wgo-agv/client/src/pages/AdminAgv/AdminNewsletterInscriptionsPage.vue
@@ -1,5 +1,19 @@
 <template>
   <q-page class="row justify-evenly">
-    <div class="col-12"></div>
+    <div class="col-12">
+      <NsLtInscriptionAdminComponent />
+    </div>
   </q-page>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import NsLtInscriptionAdminComponent from "../../components/NsLtInscriptionAdminComponent/NsLtInscriptionAdminComponent.vue";
+
+export default defineComponent({
+  name: "AdminNewsletterInscriptionsPage",
+  components: {
+    NsLtInscriptionAdminComponent,
+  },
+});
+</script>
