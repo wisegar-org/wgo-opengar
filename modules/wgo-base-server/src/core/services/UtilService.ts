2ac20c8c87f6916f6f6e9c44344312b4ff214a70
Liana Juliá <lianabeatriz93@gmail.com>
2022-12-15 15:31:45 -0500

    0.0.1-8 wgo-base-server

--- modules/wgo-base-server/src/core/services/UtilService.ts
+++ modules/wgo-base-server/src/core/services/UtilService.ts
@@ -1,4 +1,15 @@
+import currency from "currency.js";
+
 export const UtilService = {
+  roundNumber(value: number, decimal?: number) {
+    if (!value) return value;
+    const roundValue = currency(value, {
+      symbol: "",
+      separator: "'",
+      precision: decimal || 2,
+    }).format();
+    return parseFloat(roundValue);
+  },
   async readStreamData(stream: any) {
     return new Promise((res, rej) => {
       let dataStm: any[] = [];
