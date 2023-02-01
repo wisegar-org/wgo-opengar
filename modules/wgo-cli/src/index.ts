dd13658d20c9632f377925d3ca3de78cdcd1e2f0
yarielre <yariel.re@gmail.com>
2023-02-01 22:02:14 +0100

    Update cli version build

--- modules/wgo-cli/src/index.ts
+++ modules/wgo-cli/src/index.ts
@@ -4,7 +4,7 @@ import { EmptyVersion } from "./options/ICmdOptions";
 import { Logger } from "./utils/Logger";
 
 export const wgoCli = () => {
-  const current_version: string = "0.0.3-7";
+  const current_version: string = "0.0.3-8";
 
   const processVersion = process.env.npm_package_version
     ? process.env.npm_package_version
