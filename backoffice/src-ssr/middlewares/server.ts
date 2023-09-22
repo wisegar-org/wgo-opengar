8f9157dc7a3eafd185a3be6c64d2c5491ff0ba3c
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-09-22 16:56:06 +0200

    Remove debugger

--- backoffice/src-ssr/middlewares/server.ts
+++ backoffice/src-ssr/middlewares/server.ts
@@ -1,6 +1,5 @@
 import { ssrMiddleware } from "quasar/wrappers";
 import { run } from "@wisegar-org/wgo-base-server";
 export default ssrMiddleware(async ({ app }) => {
-  debugger;
   await run(app);
 });
