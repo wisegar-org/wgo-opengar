4460422d235f8a690b9c66cb54257e35f007515a
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-09-27 18:26:43 +0200

    Add AppComtroller to backoffice

---
+++ backoffice/modules/core/controllers/AppController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { Controller, Get } from "@wisegar-org/wgo-base-server";
+
+@Controller("/api")
+export class AppController {
+  @Get("/version")
+  public GetVersion(req: Request, res: Response) {
+    res.status(200).json("{version: 'v0.0.1'}");
+  }
+}
